133acdd78abb038fb2ffca530966e488f84cddd5
Kevin Stubblefield <kevin@stubblefield.io>
2022-12-07 19:39:48 -0600

    day 6

---
+++ 2022/6/a.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	f, _ := os.Open("./input.txt")
+
+	s := bufio.NewScanner(f)
+	s.Scan()
+	fmt.Println(getAnswer(s.Text()))
+}
+
+func getAnswer(input string) int {
+Outer:
+	for i, _ := range input {
+		if i < 4 {
+			continue
+		}
+
+		s := input[i-4 : i]
+		set := make(map[rune]bool)
+		for _, x := range s {
+			if _, found := set[x]; found {
+				continue Outer
+			} else {
+				set[x] = true
+			}
+		}
+
+		return i
+	}
+	return -1
+}
